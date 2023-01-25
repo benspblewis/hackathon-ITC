c0cf866eebc00e7b86dd53a44e718b54b4186d6b
benspblewis <benspblewis@googlemail.com>
2023-01-25 15:34:47 +0200

    Initial start to chat rooms

--- lingochat/src/App.js
+++ lingochat/src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Chat from './Chat';
+import IO from 'socket.io-client';
 import './App.css';
+import FindChatRoom from './FindChatRoom';
+const socket = IO.connect('http://localhost:4000');
 
 function App() {
   return (
-    <h1>Hello</h1>
-  )
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/FindRoom" element={<FindChatRoom socket={socket} />}></Route>
+          <Route path="/chat" element={<Chat socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
